8fd7c240a314f54b9541b4cfe6cec0c3897011e9
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2021-11-16 20:24:28 +0000

    test abs/rel

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment as e_direct } from 'src/environments/environment';
+import { environment as e_rel } from '../../environments/environment';
 
 @Component({
   selector: 'app-test-component',
@@ -7,12 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponentComponent implements OnInit {
   constructor() {
-    const x = 1;
+    const x = e_rel.production;
     console.log(x);
   }
 
   ngOnInit(): void {
-    const x = 1;
+    const x = e_direct.production;
     console.log(x);
   }
 }
